689ee75da7fe52e161c82629b75f245be3250d14
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-09-28 21:35:45 +0700

    set up camera

---
+++ Survival-Shooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+    Vector3 offset;
+
+    private void Start()
+    {
+        //Get offset between target and camera
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
